be5bc498f03e17c700cbcebf5aee7513feeb696f
marikodehode <marikodehode@gmail.com>
2023-11-17 09:50:41 +0100

    oppdatering

--- src/pages/nettside.jsx
+++ src/pages/nettside.jsx
@@ -9,40 +9,34 @@ export const Nettside = () => {
         <p className='textfill-nettside'>nettsider</p>
 
         <div className='column-wrapper-nettside'>
-            <a href="https://marikodehode.github.io/JS-DOM-appendChild-NY/"><div className='photoN photoN1'></div></a>
+            <a href="https://marikodehode.github.io/JS-switchcase/"><div className='photoN photoN1'></div></a>
             
-            <a className="https://marikodehode.github.io/test-bilder/"><div className='photoN photoN2'></div></a>
+            <a href="https://marikodehode.github.io/jsDOMappendChild/"><div className='photoN photoN2'></div></a>
 
-            <a className="https://marikodehode.github.io/API-vits/"><div className='photoN photoN3'></div></a>
+            <a href=""><div className='photoN photoN3'></div></a>
 
-            <a className="https://marikodehode.github.io/API-vits/"><div className='photoN photoN4'></div></a>
+            <a href="https://marikodehode.github.io/vits-api/"><div className='photoN photoN4'></div></a>
 
-            <a className="https://marikodehode.github.io/Extra-oppgave-Frontend/"><div className='photoN photoN5'></div></a>
-            <div className='photoN photoN6'></div>
+            <a href="https://marikodehode.github.io/js-DOM-lagsirkel/"><div className='photoN photoN5'></div></a>
+
+            <a href="https://marikodehode.github.io/js-switchcase-two/"><div className='photoN photoN6'></div></a>
 
 
 
-            <a href=""><div className='photoN photoN7'></div></a>
+            <a href="https://dyr.vercel.app/"><div className='photoN photoN7'></div></a>
             
-            <a className=""><div className='photoN photoN8'></div></a>
+            <a href="https://tipcalculator-bay.vercel.app/"><div className='photoN photoN8'></div></a>
 
-            <a className=""><div className='photoN photoN9'></div></a>
+            <a href="https://marikodehode.github.io/subscribe/"><div className='photoN photoN9'></div></a>
             
-            <a className=""><div className='photoN photoN10'></div></a>
+            <a href="https://marikodehode.github.io/jsDOM/"><div className='photoN photoN10'></div></a>
 
-            <a className=""><div className='photoN photoN11'></div></a>
-            <div className='photoN photoN12'></div>
+            <a href="https://marikodehode.github.io/html-oppgave-hund/"><div className='photoN photoN11'></div></a>
+
+            <a href="https://marikodehode.github.io/kalkulator-recap/"><div className='photoN photoN12'></div></a>
         </div>
 
-         <div className="tekst-nettside-bunn">
-        <h1 className="nameTitletwo-nettside">Min vei</h1>
-        <p className='textfilltwo-nettside'>Min start på den digitale verden av å lage nettsider startet i 2008 da jeg ble student ved IGM. Dette var via min arbeidsplass Kolon Reklame AS. Ønske var å lage en nettside til vår egen bedrift. Legger inn noen skjermbilder av nettsiden, denne nettsiden ble satt opp i Dreamweaver.
 
-       Flere år senere startet jeg i en bedrift hos Motivation Boxes, her bistå jeg i deres nettbutikk som grafisk designer. 
-        Mens jeg var ansatt her laget jeg to nettsider, en i wordpress til divabox.no og en i Kajabi motivationbymarion.no. Divabox skulle være en nettbutikk og motivationbymarion var et nettside med nettkurs i motivasjon. Men disse nettsidene har jeg dessverre ikke materiell å henvise til da disse selskapene ikke lenger eksiterer. 
-
-        Nå i 2023 startet jeg på et 3 måneders kurs hos Podium i koding og programmering. Hvor jeg har laget blandt annet denne nettsiden.
-        Legger ved en link til Github i bunn av nettsiden, slik at det er mulighet å se noen av våre andre oppgaver.</p> </div>
 
         </div>
         </section>
